00f613c897c23cd42c0afc35bcbc97ff2b7de569
Neil Atkinson <neil@new-bamboo.co.uk>
2015-09-08 17:33:27 +0100

    git repository locations controller checks validity before creation.

--- spec/controllers/git_repository_locations_controller_spec.rb
+++ spec/controllers/git_repository_locations_controller_spec.rb
@@ -17,4 +17,42 @@ RSpec.describe GitRepositoryLocationsController do
         git_repository_location: git_repository_location)
     }
   end
+
+  context 'when logged in', :logged_in do
+    before do
+      session[:current_user] = User.new
+    end
+
+    describe 'GET index' do
+      it 'returns http success' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'POST create' do
+      subject { post :create, params }
+
+      context 'when the GitRepositoryLocation is invalid' do
+        let(:params) {
+          { git_repository_location: { name: 'test', uri: 'git@github.com:some/other-repo.git' } }
+        }
+
+        it { is_expected.to render_template(:index) }
+
+        it 'shows an error message' do
+          post :create, params
+          expect(flash[:error]).to be_present
+        end
+      end
+
+      context 'when the GitRepositoryLocation is valid' do
+        let(:params) {
+          { git_repository_location: { name: 'test', uri: 'ssh://git@github.com/some/other-repo.git' } }
+        }
+
+        it { is_expected.to redirect_to(git_repository_locations_path) }
+      end
+    end
+  end
 end
