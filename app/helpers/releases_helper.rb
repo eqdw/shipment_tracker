5e58dc630d8be6731c65f9b99d378fa0b1bf8b37
Neil Atkinson <neil@new-bamboo.co.uk>
2015-09-15 10:28:01 +0100

    Give user feedback via tooltip on Releases page
    Explain what to expect from Feature Review links.
    
    Signed-off-by: Dennis Ideler <dennis.ideler@fundingcircle.com>

--- app/helpers/releases_helper.rb
+++ app/helpers/releases_helper.rb
@@ -1,6 +1,12 @@
 module ReleasesHelper
   def feature_review_link(feature_review)
-    path = feature_review.approved_path || feature_review.path_with_query_time
-    link_to(feature_review.approval_status, path)
+    if feature_review.approved_path
+      path = feature_review.approved_path
+      msg = 'View Feature Review at approval time'
+    else
+      path = feature_review.path_with_query_time
+      msg = 'View Feature Review when committed to master'
+    end
+    link_to(feature_review.approval_status, path, data: { toggle: 'tooltip' }, title: msg)
   end
 end
