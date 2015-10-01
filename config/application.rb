17497881d8deda1e921761b3a2c3a4d3d4e11c5d
Dennis Ideler <ideler.dennis@gmail.com>
2015-10-01 11:17:59 +0100

    Make ticket spec more agnostic of what approval states are
    We should not need to update the spec every time we add a new approval
    state as it does not give us any increased confidence.
    Instead we should be testing that #approved? works for any given
    approval state.
    
    Minor changes to approved_statuses for readability and sorting states
    into a more logical order (has no effect on the functionality).

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,6 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'sprockets/railtie'
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -36,7 +35,7 @@ module ShipmentTracker
     config.ssh_private_key = ENV['SSH_PRIVATE_KEY']
     config.ssh_public_key = ENV['SSH_PUBLIC_KEY']
     config.ssh_user = ENV['SSH_USER']
-    config.approved_statuses = ENV.fetch('APPROVED_STATUSES', 'Done,Ready for Deployment,Deployed')
+    config.approved_statuses = ENV.fetch('APPROVED_STATUSES', 'Ready for Deployment, Deployed, Done')
       .split(/\s*,\s*/)
     config.git_repository_cache_dir = Dir.tmpdir
   end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Ticket do
   describe '#approved?' do
-    it 'returns true for Done, Ready for Deployment and Deployed' do
-      ['Done', 'Ready for Deployment', 'Deployed'].each do |status|
-        expect(described_class.new(status: status).approved?).to eq(true)
+    it 'returns true for approved statuses' do
+      Rails.application.config.approved_statuses.each do |status|
+        expect(Ticket.new(status: status).approved?).to be true
       end
     end
 
     it 'returns false for any other status' do
-      expect(described_class.new(status: 'any').approved?).to eq(false)
+      expect(Ticket.new(status: 'any').approved?).to be false
     end
 
     it 'returns false if status not set' do
-      expect(described_class.new(status: nil).approved?).to eq(false)
+      expect(Ticket.new(status: nil).approved?).to be false
     end
   end
 end
