113b537a89775133f03a82d7c3bff466b73cfc8d
Dennis Ideler <dennis.ideler@fundingcircle.com>
2016-01-12 13:23:11 -0800

    Handle Heroku deploy payload
    * Map Heroku fields to standard deploy event fields
      because we cannot change parameters sent by Heroku
    
    Signed-off-by: Molly Huerster <molly.huerster@fundingcircle.com>

--- app/models/events/deploy_event.rb
+++ app/models/events/deploy_event.rb
@@ -3,7 +3,7 @@ require 'events/base_event'
 module Events
   class DeployEvent < Events::BaseEvent
     def app_name
-      details.fetch('app_name', nil).try(:downcase)
+      details.fetch('app_name', details.fetch('app', nil)).try(:downcase)
     end
 
     def server
@@ -11,11 +11,11 @@ module Events
     end
 
     def version
-      details.fetch('version', nil)
+      details.fetch('version', details.fetch('head_long', nil))
     end
 
     def deployed_by
-      details.fetch('deployed_by', nil)
+      details.fetch('deployed_by', details.fetch('user', nil))
     end
 
     def environment
@@ -29,7 +29,7 @@ module Events
     end
 
     def servers_fallback
-      [details.fetch('server', nil)].compact
+      [details.fetch('server', details.fetch('url', nil))].compact
     end
   end
 end
--- spec/models/events/deploy_event_spec.rb
+++ spec/models/events/deploy_event_spec.rb
@@ -1,14 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Events::DeployEvent do
-  subject { described_class.new(details: payload) }
+  subject { Events::DeployEvent.new(details: payload) }
 
   context 'when given a valid payload' do
+    context 'when the payload comes from Heroku' do
+      let(:payload) {
+        {
+          'app' => 'nameless-forest',
+          'user' => 'user@example.com',
+          'url' => 'http://nameless-forest.herokuapp.com',
+          'head_long' => '123abc',
+        }
+      }
+
+      it 'returns the correct values' do
+        expect(subject.app_name).to eq('nameless-forest')
+        expect(subject.server).to eq('http://nameless-forest.herokuapp.com')
+        expect(subject.version).to eq('123abc')
+        expect(subject.deployed_by).to eq('user@example.com')
+      end
+    end
+
     let(:payload) {
       {
         'app_name' => 'soMeApp',
         'servers' => ['prod1.example.com', 'prod2.example.com'],
-        'version' => '123',
+        'version' => '123abc',
         'deployed_by' => 'bob',
       }
     }
@@ -16,7 +34,7 @@ RSpec.describe Events::DeployEvent do
     it 'returns the correct values' do
       expect(subject.app_name).to eq('someapp')
       expect(subject.server).to eq('prod1.example.com')
-      expect(subject.version).to eq('123')
+      expect(subject.version).to eq('123abc')
       expect(subject.deployed_by).to eq('bob')
     end
 
@@ -25,7 +43,7 @@ RSpec.describe Events::DeployEvent do
         {
           'app_name' => 'soMeApp',
           'server' => 'uat.example.com',
-          'version' => '123',
+          'version' => '123abc',
           'deployed_by' => 'bob',
         }
       }
@@ -33,7 +51,7 @@ RSpec.describe Events::DeployEvent do
       it 'returns the correct values' do
         expect(subject.app_name).to eq('someapp')
         expect(subject.server).to eq('uat.example.com')
-        expect(subject.version).to eq('123')
+        expect(subject.version).to eq('123abc')
         expect(subject.deployed_by).to eq('bob')
       end
     end
